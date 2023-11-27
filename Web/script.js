5e0ce606ebe9e51ad205c12259893b1426696cdc
Vaibhav0120 <150184948+Vaibhav0120@users.noreply.github.com>
2023-11-27 18:33:55 +0530

    changes

--- Web/script.js
+++ Web/script.js
@@ -5,7 +5,7 @@ const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
 
 yesBtn.addEventListener("click", () => {
-  question.innerHTML = "hehe Hii :)";
+  question.innerHTML = "hehe Hii!";
   gif.src =
     "https://media.giphy.com/media/UMon0fuimoAN9ueUNP/giphy.gif";
 });
